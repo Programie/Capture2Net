4028c649e8c50d106a34ef2e072f3343d04b0a60
programie <programie@7dbd2c99-0d0f-423d-9eae-8195998ee882>
2012-12-01 00:49:00 +0000

    Replaced config.inc.php with config.sample.inc.php

---
+++ webinterface/source/includes/config.sample.inc.php
@@ -0,0 +1,13 @@
+<?php
+// MySQL
+define("MYSQL_DSN", "mysql:host=localhost;dbname=capture2net");
+define("MYSQL_USERNAME", "root");
+define("MYSQL_PASSWORD", "");
+
+// Upload target
+define("UPLOAD_PATH", "/var/www/capture2net_uploads");
+define("UPLOAD_URL", "http://example.com/capture2net_uploads");
+
+// Filenames
+define("FILENAMES_ALLOWEDCHARACTERS", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-");
+?>
