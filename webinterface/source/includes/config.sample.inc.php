606e92a60ed5d58337d7a8bb14e2655565ce0f2a
programie <programie@7dbd2c99-0d0f-423d-9eae-8195998ee882>
2012-12-01 01:20:02 +0000

    Added comments to config.sample.inc.php

--- webinterface/source/includes/config.sample.inc.php
+++ webinterface/source/includes/config.sample.inc.php
@@ -1,13 +1,20 @@
 <?php
+/**************************************
+ *                                    *
+ *      DO NOT MODIFY THIS FILE!      *
+ * CREATE A COPY NAMED config.inc.php *
+ *                                    *
+ **************************************/
+
 // MySQL
-define("MYSQL_DSN", "mysql:host=localhost;dbname=capture2net");
-define("MYSQL_USERNAME", "root");
-define("MYSQL_PASSWORD", "");
+define("MYSQL_DSN", "mysql:host=localhost;dbname=capture2net");// Hostname and database
+define("MYSQL_USERNAME", "root");// Username
+define("MYSQL_PASSWORD", "");// Password
 
 // Upload target
-define("UPLOAD_PATH", "/var/www/capture2net_uploads");
-define("UPLOAD_URL", "http://example.com/capture2net_uploads");
+define("UPLOAD_PATH", "/var/www/capture2net_uploads");// Path where the screenshots should be saved
+define("UPLOAD_URL", "http://example.com/capture2net_uploads");// URL to the directory specified in UPLOAD_PATH
 
 // Filenames
-define("FILENAMES_ALLOWEDCHARACTERS", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-");
+define("FILENAMES_ALLOWEDCHARACTERS", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-");// Character map with valid characters in filenames (Any other character will be replaced with the underscore)
 ?>
